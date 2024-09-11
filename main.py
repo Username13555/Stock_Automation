c69d0469c9f12d735d5ff6e3cf16bf90cefb6657
Username13555 <triphopl1@gmail.com>
2024-09-10 21:57:31 -0700

    add lines related to modules

---
+++ main.py
@@ -0,0 +1,5 @@
+import yfinance as yf # Module to get stock data
+import pandas as pd # Module to manipulate data
+import openpyxl # Module to write data to Excel
+import datetime # Module to get the current date
+import os # Module to check if a file exists
